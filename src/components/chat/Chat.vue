f10e36faea73fdcb11290535cf31be4b76d7f890
Vladislav Tolkachev <tvladick1996@yandex.ru>
2022-10-17 23:34:31 +0300

    фильтр поиска

--- src/components/chat/Chat.vue
+++ src/components/chat/Chat.vue
@@ -1,29 +1,18 @@
 <template>
-    <div class="chat">
-        <slot />
-    </div>
+  <div class="chat">
+      <slot />
+  </div>
 </template>
 
-<script>
-export default {
-  props: {
-    dialogs: {
-      type: Array,
-      required: true,
-    },
-  },
-};
-</script>
-
 <style scoped>
-  .chat {
-    display: flex;
-    width: 100%;
-    height: 100%;
-    font-family: "Raleway";
-    font-size: 14px;
-    border: 1px solid var(--border-color);
-    border-radius: 4px;
-    background-color: #fff;
-  }
+.chat {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  font-family: "Raleway";
+  font-size: 14px;
+  border: 1px solid var(--border-color);
+  border-radius: 4px;
+  background-color: #fff;
+}
 </style>
